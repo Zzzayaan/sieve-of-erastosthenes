2077a29f948312623c043ded0269dfb1c98bf4b4
Zayaan <zayaan737@gmail.com>
2022-02-10 21:32:39 +0000

    Updated comments

--- sieve_of_eratosthenes.py
+++ sieve_of_eratosthenes.py
@@ -2,8 +2,8 @@ def sieve_of_erosthenes(n):
 	prime_list = [] #Creates an empty list
 	for i in range (2, n+1): #For loop iterates through 1st prime (2) up to and including n
 		if i not in prime_list: #Condition check: detects numbers that should be printed
-			print(i) #Prints a prime factor
+			print(i) #Prints a prime number
 			for j in range (i*i, n+1, i): #For loop iterating from i*i up to and including n every +i
-				prime_list.append(j) #Adds all numbers multiples of a prime factor -> loop restarts 
+				prime_list.append(j) #Appends numbers multiples of the prime number i -> loop restarts 
 
 print(sieve_of_erosthenes(10000))
